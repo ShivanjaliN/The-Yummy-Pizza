95f8301e77b4c84af03cc477c872991aa603a2ed
Shivanjali <shiva1296@gmail.com>
2020-05-06 10:35:03 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -23,9 +23,9 @@ class App extends Component {
   render() {
     return (
     	<Router>
-    		<nav className="navbar navbar-expand-lg App-header">
+    		<nav className="navbar navbar-expand-lg navbar-light bg-light">
 			  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-			    <span className="navbar-toggler-icon"></span>
+			    <span className="navbar-toggler-icon  text-black"></span>
 			  </button>
 			  <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
 			  	<span className="logo d-none d-sm-block" >{logo}</span>
