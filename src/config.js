f789d068fd8173dfaf666ce2a02c795ef9270804
Shivanjali <shiva1296@gmail.com>
2020-05-06 09:26:42 +0530

    updated config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = 'https://yummy-pizza-backend.herokuapp.com/api';
 
-export const ASSET_BASE_URL = 'https://yummy-pizza-backend.herokuapp.com/images/';
+export const ASSET_BASE_URL = 'https://yummy-pizza-backend.herokuapp.com/assets/images/';
